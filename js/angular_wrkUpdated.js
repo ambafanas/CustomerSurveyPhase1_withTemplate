5943482c96c6187b69ed630194f11224a1813430
pranaykadu <kpranay@gmail.com>
2015-11-04 22:54:27 +0530

    base64 and constant for rest API

--- js/angular_wrkUpdated.js
+++ js/angular_wrkUpdated.js
@@ -13,7 +13,7 @@
 			     
 				myVar: function ($q,surveyService,$timeout,$http) {
 					var defer = $q.defer();
-					var url='http://localhost:8080/RESTfulProject/REST/WebService/GetOraganizations' 
+					var url= surveyService.getRestAPIURL() + 'GetOraganizations' 
 					var req={
 				 	method: 'GET',
 				 	url: url,
@@ -45,7 +45,7 @@
 			     
 				myVar: function ($q,surveyService,$timeout,$http) {
 					var defer = $q.defer();
-					var url='http://localhost:8080/RESTfulProject/REST/WebService/GetFirmDetails/' + surveyService.getUserID() ;
+					var url=surveyService.getRestAPIURL() +'GetFirmDetails/' + surveyService.getUserID() ;
 					var req={
 				 	method: 'GET',
 				 	url: url,
@@ -93,6 +93,11 @@ myApp.factory('surveyService', function() {
   		var FirmsData='';
   		var FirmID='';
 		var OrganizationNames='';
+		var RestAPIURL='http://localhost:8080/RESTfulProject/REST/WebService/';
+		var getRestAPIURL = function(){
+     		return RestAPIURL;
+  		};
+
 		var setOrganizationNames = function(org) {
      		OrganizationNames=org.data;
   		};
@@ -140,7 +145,8 @@ myApp.factory('surveyService', function() {
     		saveFirmID:saveFirmID,
     		getFirmID:getFirmID,
     		setOrganizationNames:setOrganizationNames,
-    		getOrganizationNames:getOrganizationNames
+    		getOrganizationNames:getOrganizationNames,
+    		getRestAPIURL:getRestAPIURL
 
   		};
 	});
@@ -153,7 +159,7 @@ myApp.controller('SurveyForm',['$http','$scope','$location','surveyService', fun
 		$scope.SurveyForm.userID= surveyService.getUserID();
     	var req={
  	method: 'POST',
- 	url: 'http://localhost:8080/RESTfulProject/REST/WebService/WriteSurveyData',
+ 	url: surveyService.getRestAPIURL()+'WriteSurveyData',
  	headers: {
   		  'Content-Type': 'application/json'}, data:angular.toJson(alldata)
    };
@@ -185,11 +191,21 @@ myApp.controller('CustomerSurvey',['$http','$timeout','$scope','$location','surv
 		{  
 			
 			
-			var url='http://localhost:8080/RESTfulProject/REST/WebService/GetLoginDetails/' + user.email +'/'+ user.password +'/' + user.Organization
+			
 			
 						$scope.form1_data=angular.copy(user);
 						//surveyService.addSurvey($scope.form1_data);
 						$scope.test1= angular.toJson(user);
+						var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+						
+						// Encode the String
+						var encodedString = Base64.encode(user.password);
+						alert(encodedString);
+						// Decode the String
+						var decodedString = Base64.decode(encodedString);
+						alert(decodedString);
+						var url=surveyService.getRestAPIURL() + 'GetLoginDetails/' + user.email +'/'+encodedString +'/' + user.Organization
 				 var req={
 				 	method: 'GET',
 				 	url: url,
